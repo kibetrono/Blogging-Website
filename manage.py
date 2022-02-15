83974ac5c27d01a14483bb866a8b35d1950f9319
email <kibetdavidro@gmail.com>
2022-02-15 09:25:16 +0300

    update manage file

--- manage.py
+++ manage.py
@@ -4,5 +4,26 @@ from app.models import User,Post,Role,Comment
 from  flask_migrate import Migrate, MigrateCommand
 
 
+app = create_app('development')
+migrate = Migrate(app,db)
+manager = Manager(app)
+manager.add_command('server', Server)
+manager.add_command('db',MigrateCommand)
+
+
+@manager.command
+def test():
+    """Run the unit tests"""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role,Post=Post,Comment=Comment)
+
+
+
 if __name__ == '__main__':
     manager.run()
