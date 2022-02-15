2f653f685ffb0ebdc31339c9c479d34c1099bce4
email <kibetdavidro@gmail.com>
2022-02-15 09:28:11 +0300

    update prodconfig file

--- config.py
+++ config.py
@@ -13,3 +13,14 @@ class Config:
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
+class ProdConfig(Config):
+    """Production configuration child class
+    Args:
+    Config: The parent configuration class with General configuration settings
+    """
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kibet:KibetFlask@localhost/ownblog'
+
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")  # or other relevant config var
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+    # rest of connection code using the connection string `uri`
